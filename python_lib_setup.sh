5c74332d389a07254bb04e9182b3b20afbefa4c3
Hiroaki Shioi <shioin@hotmail.com>
2015-05-27 09:11:04 -0400

    remove files after 0527

--- python_lib_setup.sh
+++
@@ -1,9 +0,0 @@
-# Install essentials, numpy, matplot
-sudo apt-get install build-essential python-dev
-sudo apt-get install python-numpy
-sudo apt-get install python-matplotlib
-
-# Install Pandas, Pydoop
-pip install pandas
-pip install pydoop
-
