41edc6f497b5f4afb4cec90f97f3539cf72d74dd
Hiroaki Shioi <shioin@hotmail.com>
2015-06-20 21:53:03 +0900

    use "cm launcher" instead of heat stack-create
    use "cm launcher" instead of heat stack-create

--- cloudmesh_python_analyze/cloudmesh_python_analyze/command_python_analyze.py
+++ cloudmesh_python_analyze/cloudmesh_python_analyze/command_python_analyze.py
@@ -26,17 +26,21 @@ class command_python_analyze(object):
         os.system("eval $(ssh-agent -s)")
         os.system("ssh-add ~/.ssh/id_rsa")
 
-        # Run heat command to create virtual clusters
+        # Run cm launcher command to create hadoop clusters
+        os.system('cm launcher pydoop-cluster-airline')
 
-        os.system("heat stack-create -u https://raw.githubusercontent.com/futuresystems/465-project-git4hiroaki-benshadburne-yujinhirose/master/hadoop-cluster-airline-del-pred.yaml -P "KeyName=hshioi-india-key;PublicKeyString=`cat ~/.ssh/id_rsa.pub`;PrivateKeyString=`cat ~/.ssh/id_rsa`" heat-hadoop-cluster-$PORTALNAME")
+
+        # Run heat command to create virtual clusters
+        # os.system("heat stack-create -u https://raw.githubusercontent.com/futuresystems/465-project-git4hiroaki-benshadburne-yujinhirose/master/hadoop-cluster-airline-del-pred.yaml -P "KeyName=hshioi-india-key;PublicKeyString=`cat ~/.ssh/id_rsa.pub`;PrivateKeyString=`cat ~/.ssh/id_rsa`" heat-hadoop-cluster-$PORTALNAME")
+        # os.system("heat stack-create -u https://raw.githubusercontent.com/futuresystems/465-project-git4hiroaki-benshadburne-yujinhirose/5eaa96873642d7b7499cc04a77cf6ca2d5af9f7d/hadoop-cluster-airline-del-pred.yaml -P "KeyName=hshioi-india-key;PublicKeyString=`cat ~/.ssh/id_rsa.pub`;PrivateKeyString=`cat ~/.ssh/id_rsa`" heat-hadoop-cluster-$PORTALNAME")
+
 
         # If user input the location of public/private key
-        name = arguments['NAME'] # Arbitrary name e.g., heat-hadoop-cluster-hshioi0620'
-        public_key = arguments['--public_key'] # ~/.ssh/id_rsa.pub
-        private_key = arguments['--private_key'] # ~/.ssh/id_rsa
-        key_name = arguments['--key_name'] # should be resgistered in OpenStack
-
-        os.system('heat stack-create -u https://raw.githubusercontent.com/futuresystems/465-project-git4hiroaki-benshadburne-yujinhirose/master/hadoop-cluster-airline-del-pred.yaml -P "KeyName=' + key_name +';PublicKeyString=`cat ' + public_key + '`;PrivateKeyString=`cat ' + private_key + '` ' + name)
+        # name = arguments['NAME'] # Arbitrary name e.g., heat-hadoop-cluster-hshioi0620'
+        # public_key = arguments['--public_key'] # ~/.ssh/id_rsa.pub
+        # private_key = arguments['--private_key'] # ~/.ssh/id_rsa
+        # key_name = arguments['--key_name'] # should be resgistered in OpenStack
+        # os.system('heat stack-create -u https://raw.githubusercontent.com/futuresystems/465-project-git4hiroaki-benshadburne-yujinhirose/master/hadoop-cluster-airline-del-pred.yaml -P "KeyName=' + key_name +';PublicKeyString=`cat ' + public_key + '`;PrivateKeyString=`cat ' + private_key + '` ' + name)
 
 
         return 1
