06d9eaf4beb5554533333184a66b90e68be6ec6f
benshadburne <bhshad@gmail.com>
2015-06-25 16:46:42 -0400

    changed method of bash script, simplified it by using a config file for ease of use

---
+++ config.sh
@@ -0,0 +1,3 @@
+HEATUSERNAME=YourUsernameHere
+HEATPUBKLOC=~/.ssh/id_rsa.pub
+HEATPVTKLOC=~/.ssh/id_rsa
--- start-clusters.sh
+++ start-clusters.sh
@@ -1,16 +1,8 @@
+source config.sh
 
-read -p "What is your Username?: " Heat_Username
-Hadoop1Name="Hadoop1-$Heat_Username"
-Hadoop2Name="Hadoop2-$Heat_Username"
-Hadoop3Name="Hadoop3-$Heat_Username"
+echo Creating Stack for $HEATUSERNAME
 
-read -p "Type Path to your PublicKey. (i.e. ~/.ssh/publickey): " Heat_PublicKey
-Heat_PublicKey='cat $Heat_PublicKey'
-read -p "Type Path to your PrivateKey. (i.e. ~/.ssh/privatekey): " Heat_PrivateKey
-Heat_PrivateKey='cat $Heat_PrivateKey'
+heat stack-create --template-file hadoop-cluster-airline-del-pred.yaml -P "KeyName=id_rsa;PublicKeyString='cat $HEATPUBKLOC';PrivateKeyString='cat $HEATPVTKLOC';Hadoop1Name="Hadoop1-$HEATUSERNAME";Hadoop2Name="Hadoop2-$HEATUSERNAME";Hadoop3Name="Hadoop3-$HEATUSERNAME"" flight-analysis-hadoop-cluster-$HEATUSERNAME
 
-echo Creating Heat Stack for $Heat_Username
-
-heat stack-create --template-file hadoop-cluster-airline-del-pred.yaml -P "KeyName=id_rsa;PublicKeyString=$Heat_PublicKey;PrivateKeyString=$Heat_PrivateKey;Hadoop1Name=$Hadoop1Name;Hadoop2Name=$Hadoop2Name;Hadoop3Name=$Hadoop3Name" flight-analysis-hadoop-cluster-$Heat_Username
 
 
