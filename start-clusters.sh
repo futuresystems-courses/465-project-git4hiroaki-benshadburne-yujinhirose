2268cc3fd6e2ef9846b4848e45312ff8ac536f33
git4hiroaki <shioin@hotmail.com>
2015-06-26 22:04:33 +0900

    Add notes for KeyName
    Add notes for KeyName

--- start-clusters.sh
+++ start-clusters.sh
@@ -2,6 +2,10 @@ source config.sh
 
 echo Creating Stack for $HEATUSERNAME
 
+# As "KeyName", you need to check what name your public key is registered in India
+# Please make sure to match the belwo finger prints
+# 1. ssh-keygen -lf ~/.ssh/id_rsa.pub
+# 2. nova keypair-list
 heat stack-create --template-file hadoop-cluster-airline-del-pred.yaml -P "KeyName=id_rsa;PublicKeyString='cat $HEATPUBKLOC';PrivateKeyString='cat $HEATPVTKLOC';Hadoop1Name="Hadoop1-$HEATUSERNAME";Hadoop2Name="Hadoop2-$HEATUSERNAME";Hadoop3Name="Hadoop3-$HEATUSERNAME"" flight-analysis-hadoop-cluster-$HEATUSERNAME
 
 
