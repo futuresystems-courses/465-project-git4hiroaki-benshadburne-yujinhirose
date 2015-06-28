1c78f350bc38aafd09bf17561f142393d92bb581
git4hiroaki <shioin@hotmail.com>
2015-06-28 14:22:02 +0900

    typo

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #read -p "Please input your KeyName: " KeyName
 KeyName=hshioi-ubuntu-vm-key
 HEATUSERNAME=$OS_USERNAME
-ssh-kegen -f temp.rsa -t -N ''
+ssh-keygen -f temp.rsa -t -N ''
 HEATPUBKLOC=./temp.rsa.pub
 HEATPVTKLOC=./temp.rsa
 
